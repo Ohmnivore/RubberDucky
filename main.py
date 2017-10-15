126658f84f0dd9c14decc4bf764d070f40e4793a
Mark Beiline <markbeiline@gmail.com>
2017-10-15 01:42:18 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import glfw
+from OpenGL.GL import *
+
+def main():
+    # Initialize the library
+    if not glfw.init():
+        return
+    # Create a windowed mode window and its OpenGL context
+    window = glfw.create_window(1024, 720, "RubberDucky", None, None)
+    if not window:
+        glfw.terminate()
+        return
+
+    # Make the window's context current
+    glfw.make_context_current(window)
+
+    glClearColor(1, 1, 1, 1)
+
+    # Loop until the user closes the window
+    while not glfw.window_should_close(window):
+        # Render here, e.g. using pyOpenGL
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        # Swap front and back buffers
+        glfw.swap_buffers(window)
+
+        # Poll for and process events
+        glfw.poll_events()
+
+    glfw.terminate()
+
+if __name__ == "__main__":
+    main()
