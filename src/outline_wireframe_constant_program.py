31db6f7273d9e26fee5dc6fa1dbd1444e016690e
Mark Beiline <markbeiline@gmail.com>
2017-10-21 02:39:13 -0400

    Better helpful comment

--- src/outline_wireframe_constant_program.py
+++ src/outline_wireframe_constant_program.py
@@ -45,7 +45,7 @@ class OutlineWireframeConstantProgram(Program):
         glUniformMatrix4fv(self.uProjection, 1, GL_FALSE, camera.projection.astype('float32').tobytes())
         model_view = camera.view * model.model
         glUniformMatrix4fv(self.uTransposeInverseModel, 1, GL_FALSE, model_view.inverse.transpose().astype('float32').tobytes())
-        glUniform1f(self.uOutlineWidth, px_size * self.line_width / 2.0) # Offset by half the line width
+        glUniform1f(self.uOutlineWidth, px_size * self.line_width / 2.0) # Offset by half the line width, as it's normally drawn along the middle of the edges
         self.main_program.render_meshmtls(self.render_meshmtl, model, opaque)
 
         glDisable(GL_STENCIL_TEST)
